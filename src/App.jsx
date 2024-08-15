cf3dd26766f92eb6d4ade039cfe4d35289c74c09
rbrickmn <rbrickmn@gmail.com>
2024-08-15 16:45:50 -0400

    Add header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import { useState } from "react";
+import axios from "axios";
 import "./App.css";
+
+// Component imports
+import Header from "./components/Header/Header.jsx";
 
 function App() {
   return (
     <>
-      <div className="container"></div>
+      <Header />
+      <div className="container">
+        
+      </div>
     </>
   );
 }
